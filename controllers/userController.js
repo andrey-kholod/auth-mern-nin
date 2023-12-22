6187a45ff3aefdbad15b43bbd2210ade74f24343
andrey <andrejholod77@gmail.com>
2023-12-22 12:50:11 +0300

    lesson_3_jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,11 @@
 import User from '../models/userModel.js'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const createToken = (_id) => {
+   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
+}
 
 //login user
 const loginUser = async (req, res) => {
@@ -12,13 +19,15 @@ const signupUser = async (req, res) => {
 
     try {
         const user = await User.signup(email, password)
-        console.log(user)
 
-        res.status(201).json({email, user})
+        //create a token
+        const token = createToken(user._id)
+
+        res.status(201).json({ email, user, token })
     }
-    catch(e) {
+    catch (e) {
         console.log(e)
-        res.status(403).json({error: e.message})
+        res.status(403).json({ error: e.message })
     }
 }
 
